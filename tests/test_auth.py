eb62c88aabcd12c1fcc155501dc6528a7de5e26d
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-05-07 16:11:24 -0400

    Add login succed and fail tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,6 +1,10 @@
 import json
 import re
-from models.user_model import User
+
+
+uuid_regex = (
+    r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
+)
 
 
 def helper(json_info)->any:
@@ -18,10 +22,6 @@ def test_register_user(app, client):
     td_name = "Fabian Piñeiro"
     td_email = "fabian@gmail.com"
     td_password = "123qwe"
-
-    uuid_regex = (
-        r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
-    )
 
     response = client.post('/user/register', data=json.dumps(dict(
         name=td_name,
@@ -55,4 +55,39 @@ def test_registered_with_already_registered_user(client):
     assert response.status_code == 409
     assert response.content_type == 'application/json'
     assert json_info["message"] == "User already exists. Please Log in."
-    assert json_info["status"] == "fail"
+    assert json_info["success"] is False
+
+
+def test_user_login(client):
+
+    td_email = "roberto@gmail.com"
+    td_password = "123qwe"
+    response = client.post('/user/login', data=json.dumps(dict(
+    email=td_email,
+    password=td_password
+    )), mimetype='application/json')
+
+    json_info = helper(response.response)
+
+    assert response.status_code == 202
+    assert response.content_type == 'application/json'
+    assert json_info["message"] == "Logged successfully"
+    assert re.match(uuid_regex, json_info["data"]["id"])
+    assert json_info["success"] is True
+
+
+def test_user_login_fail(client):
+
+    td_email = "fabian@gmail.com"
+    td_password = "123qwe"
+    response = client.post('/user/login', data=json.dumps(dict(
+    email=td_email,
+    password=td_password
+    )), mimetype='application/json')
+
+    json_info = helper(response.response)
+
+    assert response.status_code == 202
+    assert response.content_type == 'application/json'
+    assert json_info["message"] == "Logged fail"
+    assert json_info["success"] is False
