e12cc159bc0f87b3c8e02065df71610dd344ddb5
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-05-06 20:06:29 -0400

    Add UUID as id

--- models/user_model.py
+++ models/user_model.py
@@ -9,15 +9,17 @@ from config import instashare
 
 class User(db.Model):
     """ User Model for storing user related details """
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id = db.Column(db.String(255), primary_key=True, unique=True, nullable=False)
+    name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, email, password):
-        self.id = uuid.uuid4()
+    def __init__(self, name, email, password):
+        self.id = str(uuid.uuid4())
+        self.name = name
         self.email = email
         self.password = bcrypt.generate_password_hash(
             password, instashare.app.config.get('BCRYPT_LOG_ROUNDS')
@@ -27,10 +29,11 @@ class User(db.Model):
 
     def __repr__(self):
         location_object = {
-            'id': self.id,
-            'email': self.email,
-            'password': self.password,
-            'registered_on': self.registered_on,
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "password": self.password,
+            "registered_on": self.registered_on
         }
         return json.dumps(location_object)
 
