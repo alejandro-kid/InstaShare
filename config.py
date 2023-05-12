2a2bb4b632a00221b8eb9d75a10bddef6bf43120
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-05-12 10:59:48 -0400

    Change to load credentials with environment variables

--- config.py
+++ config.py
@@ -8,6 +8,14 @@ DB_PASS = os.getenv("DB_PASS", "postgres")
 DB_NAME = os.getenv("DB_NAME", "instashare")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 
+service_account_info = {
+    "type": "service_account",
+    "project_id": os.getenv("PROYECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY"),
+    "client_email": os.getenv("CLIENT_EMAIL"),
+}
+
 # configuration
 instashare.app.config["SQLALCHEMY_DATABASE_URI"] = (
     "postgresql+psycopg2://" + f"{DB_PASS}:{DB_USER}@{DB_HOST}/{DB_NAME}"
@@ -15,8 +23,6 @@ instashare.app.config["SQLALCHEMY_DATABASE_URI"] = (
 instashare.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
 instashare.app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "neO1Bhfajt")
 instashare.app.config["BUCKET"] = os.getenv("BUCKET", "insta-share-store")
-instashare.app.config["GOOGLE_APPLICATION_CREDENTIALS"] = \
-    os.getenv("GOOGLE_APPLICATION_CREDENTIALS", \
-    instashare.app.root_path + "/instashare-credentials.json")
+instashare.app.config["SERVICE_ACCOUNT_INFO"] = service_account_info
 
 instashare.add_api('swagger.yml')
