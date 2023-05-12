03455a3b56846087c6e23ff7c65f6a0e0910817a
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-05-12 18:55:58 -0400

    Remove Google configuration

--- config.py
+++ config.py
@@ -1,21 +1,13 @@
 import connexion
 import os
 
+
 instashare = connexion.App(__name__, specification_dir='./swagger')
 
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASS = os.getenv("DB_PASS", "postgres")
 DB_NAME = os.getenv("DB_NAME", "instashare")
 DB_HOST = os.getenv("DB_HOST", "localhost")
-
-service_account_info = {
-    "type": "service_account",
-    "project_id": os.getenv("PROYECT_ID"),
-    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-    "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),
-    "client_email": os.getenv("CLIENT_EMAIL"),
-    "token_uri": "https://oauth2.googleapis.com/token"
-}
 
 # configuration
 instashare.app.config["SQLALCHEMY_DATABASE_URI"] = (
@@ -24,6 +16,9 @@ instashare.app.config["SQLALCHEMY_DATABASE_URI"] = (
 instashare.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
 instashare.app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "neO1Bhfajt")
 instashare.app.config["BUCKET"] = os.getenv("BUCKET", "insta-share-store")
-instashare.app.config["SERVICE_ACCOUNT_INFO"] = service_account_info
+
+instashare.app.config["CELERY_BROKER_URL"] = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
+instashare.app.config["CELERY_RESULT_BACKEND"] = \
+    os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0")
 
 instashare.add_api('swagger.yml')
