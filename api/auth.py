dea6d9bc17df5df2ed44d64a8a90ecef2c0dc0f5
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-05-13 11:35:54 -0400

    Implement validation using jsonschema

--- api/auth.py
+++ api/auth.py
@@ -1,31 +1,37 @@
-from flask import Response, request, json
-from models.user_model import User
-from db_config import db
+import json
+import jsonschema
 
+from db_config import db
+from flask import Response, request
+from models.user_model import User
+from schemas.user_schema import register_user_schema, login_user_schema
 
 def register_user():
-    request_data = request.get_json()
-    user = User.query.filter_by(email=request_data["email"]).first()
-    if not user:
-        try:
-            user = User(\
-                request_data["name"],request_data["email"], request_data["password"]
-            )
-            db.session.add(user)
-            db.session.commit()
-            data = {
-                "success": True,
-                "message": "User registered successfully", 
-                "data": {
-                    "id": user.id,
-                    "name": user.name,
-                    "email": user.email
+    try:
+        request_data = request.get_json()
+        jsonschema.validate(request_data, register_user_schema)
+
+        user = User.query.filter_by(email=request_data["email"]).first()
+        if not user:
+                user = User(\
+                    request_data["name"],request_data["email"], request_data["password"]
+                )
+                db.session.add(user)
+                db.session.commit()
+                data = {
+                    "success": True,
+                    "message": "User registered successfully",
+                    "data": {
+                        "id": user.id,
+                        "name": user.name,
+                        "email": user.email
+                    }
                 }
-            }
-            response = Response(json.dumps(data), status=201, \
-                mimetype="application/json")
-        except Exception as e:
-            response = Response(e, status=400, mimetype="application/json")
+                response = Response(json.dumps(data), status=201, \
+                    mimetype="application/json")
+
+    except jsonschema.exceptions.ValidationError as exc:
+        response = Response(str(exc.message), 400, mimetype="application/json")
     else:
         data = {
             "success": False,
@@ -37,19 +43,29 @@ def register_user():
 
 
 def login():
-    request_data = request.get_json()
-    user = User.query.filter_by(email=request_data["email"]).first()
-    if user:
-        if user.check_password(request_data["password"]):
-            data = {
-                "success": True,
-                "message": "Logged successfully",
-                "data": {
-                    "id": user.id
+    try:
+        request_data = request.get_json()
+        jsonschema.validate(request_data, login_user_schema)
+
+        user = User.query.filter_by(email=request_data["email"]).first()
+        if user:
+            if user.check_password(request_data["password"]):
+                data = {
+                    "success": True,
+                    "message": "Logged successfully",
+                    "data": {
+                        "id": user.id
+                    }
                 }
-            }
-            response = Response(json.dumps(data), status=202, \
-                mimetype="application/json")
+                response = Response(json.dumps(data), status=202, \
+                    mimetype="application/json")
+            else:
+                data = {
+                    "success": False,
+                    "message": "Logged fail"
+                }
+                response = Response(json.dumps(data), status=202, \
+                    mimetype="application/json")
         else:
             data = {
                 "success": False,
@@ -57,11 +73,8 @@ def login():
             }
             response = Response(json.dumps(data), status=202, \
                 mimetype="application/json")
-    else:
-        data = {
-            "success": False,
-            "message": "Logged fail"
-        }
-        response = Response(json.dumps(data), status=202, \
-            mimetype="application/json")
+
+    except jsonschema.exceptions.ValidationError as exc:
+        response = Response(str(exc.message), 400, mimetype="application/json")
+
     return response
